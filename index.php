b428fd5d5eaeef9dc7ae56718a5fe4f4d9593451
cedricbb <hitokiri.angel@gmail.com>
2017-04-13 22:11:08 +0200

    debut du projet

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Legacy Of Elements</title>
+		<meta name="Content-Type" content="text/html">
+		<meta name="Content-Language" content="fr">
+		<meta charset="UTF-8">
+    
+    	<link rel="stylesheet" type="text/css" href="cssGen/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="cssGen/gen.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+
+	<body>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12"><h1 class="text-center titleGradient">Legacy Of Elements</h1></div>
+				<div class="col-sm-offset-4 col-sm-4 text-center"><?php include "wind.php";?><?php include "drop.php";?><?php include "flash.php";?><?php include "flame.php";?><?php include "leaf.php";?></div>
+			</div>
+
+			<div class="divider"></div>
+
+			<div class="row">
+				<div class="col-sm-offset-4 col-sm-4" id="boxInscription">
+					<div id="titleInscription"><h2>Inscription</h2></div>
+					<div id="mainInscription">
+						<form>
+							<div class="form-group">
+								<label class="text-center" for="email">Email</label>
+								<input type="text" name="email" class="form-control">
+							</div>
+
+							<div class="form-group">
+								<label class="text-center" for="mdp">Mot de passe</label>
+								<input type="text" name="mdp" class="form-control">
+							</div>
+
+							<!-- <div class="form-group">
+								<label class="text-center" for="nomPerso">Nom du personnage</label>
+								<input type="text" name="nomPerso" class="form-control">
+							</div> -->
+
+							<div class="form-group text-center">
+								<input type="submit" name="btnSub" value="Inscription" class="btn btn-success btn-lg">
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<!-- <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
+
+	</body>
+</html>
