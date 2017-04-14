5cee4a6dac2098f40efd6913e846d2231d8f1dd3
cedricbb <hitokiri.angel@gmail.com>
2017-04-14 16:42:54 +0200

    petite modification

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
    $('#carte').click(function () {
-       $('.interface').css("display", "none");
+       $('.default').css("display", "none");
        $('.monture').css("display", "none");
        $('.carte').css("display", "block");
    });
     $('#monture').click(function () {
-        $('.interface').css("display", "none");
+        $('.dafault').css("display", "none");
         $('.carte').css("display", "none");
         $('.monture').css("display", "block");
     })
