48a7bf934c3b52e109ae6530a0ffdf3f5fecbd30
BruceLyon <marotte.bruce@gmail.com>
2017-04-19 14:52:50 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -8,48 +8,12 @@
 		<meta charset="UTF-8">
    		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     	<link rel="stylesheet" type="text/css" href="cssGen/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="cssGen/gen.css">
-	</head>
-
-	<body>
-
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-12"><h1 class="text-center titleGradient"><span class="legacy">Legacy</span><img src="images/logo.png" class="logo" alt="logo" title="Logo"><span class="f">f</span>Elements</h1></div>
-			</div>
-
-			<div class="divider"></div>
-
-			<div class="row">
-				<div class="col-sm-offset-4 col-sm-4" id="boxInscription">
-					<div id="titleInscription"><h2>Inscription</h2></div>
-					<div id="mainInscription">
-						<form>
-							<div class="form-group">
-								<label class="text-center" for="email">Email</label>
-								<input type="text" name="email" class="form-control">
-							</div>
-
-							<div class="form-group">
-								<label class="text-center" for="mdp">Mot de passe</label>
-								<input type="text" name="mdp" class="form-control">
-							</div>
-
-							<!-- <div class="form-group">
-								<label class="text-center" for="nomPerso">Nom du personnage</label>
-								<input type="text" name="nomPerso" class="form-control">
-							</div> -->
-
-							<div class="form-group text-center">
-								<input type="submit" name="btnSub" value="Inscription" class="btn btn-success btn-lg">
-							</div>
-						</form>
-					</div>
-      <img src="images/cadre.png" class="cadre" alt="cadre" title="cadre">
 		<link rel="stylesheet" type="text/css" href="cssGen/index.css">
+	</head>	
 
+
+<body>
 <img src="images/cadre.png" class="cadre" alt="cadre" title="cadre">
-
 
 	<div class="container">
 
@@ -79,7 +43,7 @@
 				<div id="titleInscription"><h2>Connexion</h2>
 				</div>
 				<div id="mainInscription">
-					<form>
+					<form method="post" action="interface.php">
 
 						<div class="form-group">
 							<label class="text-center" for="email">Pseudonyme</label>
@@ -200,6 +164,15 @@
 
 	</div>
 
+
+
+
+
+<!--/!\ APPEL AUX SCRIPTS-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+
+
 <footer>
 	
 	<div class="container">
@@ -223,76 +196,21 @@
 					</ul>
 				</div>
 			</div>
-            <div class="container">
-                <div id="carousel" class="carousel slide" data-ride="carousel">
-                    <!--indicateurs visuels-->
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel" data-slide-to="1"></li>
-                        <li data-target="#carousel" data-slide-to="2"></li>
-                        <li data-target="#carousel" data-slide-to="3"></li>
-                    </ol>
-                    <div class="carousel-inner" role="listbox">
-                        <!--slide 1-->
-                        <div class="item active">
-                            <img src="https://images6.alphacoders.com/601/601152.jpg">
-                        </div>
 
-                        <!--slide 2-->
-                        <div class="item">
-                            <img src="https://images2.alphacoders.com/601/601148.jpg">
-                        </div>
+			<div class="col-sm-12 col-md-3">
+				<div class="reseaux">
+					<ul>
+						<li><a href="#"><span class="fa fa-twitter"></span>Suivez-nous sur Twitter!</a></li>
+						<li><a href="#"><span class="fa fa-facebook"></span>Et sur Facebook!</a></li>			
+					</ul>
+				</div>
+			</div>
 
-                        <!--slide 3-->
-                        <div class="item">
-                            <img src="http://inspirationseek.com/wp-content/uploads/2016/03/Horizon-Zero-Dawn-Wallpaper.jpg">
-                        </div>
 
-                        <!--slide 4-->
-                        <div class="item">
-                            <img src="http://inspirationseek.com/wp-content/uploads/2016/03/Horizon-Zero-Dawn-Wallpaper-For-Desktop.jpg">
-                        </div>
-
-                        <a href="#carousel" class="left carousel-control" role="button" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                            <span class="sr-only">Précédent</span>
-                        </a>
-
-                        <a href="#carousel" class="right carousel-control" role="button" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <span class="sr-only">Suivant</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
 		</div>
+	</div>
 
+</footer>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-			<div class="col-sm-12 col-md-3">
-				<div class="reseaux">
-					<ul>
-						<li><a href="#"><span class="fa fa-twitter"></span>Suivez-nous sur Twitter!</a></li>
-						<li><a href="#"><span class="fa fa-facebook"></span>Et sur Facebook!</a></li>			
-					</ul>
-				</div>
-			</div>
-
-
-		</div>     
-
-			<div class="col-sm-12 col-md-3">
-				<div class="reseaux">
-					<ul>
-						<li><a href="#"><span class="fa fa-twitter"></span>Suivez-nous sur Twitter!</a></li>
-						<li><a href="#"><span class="fa fa-facebook"></span>Et sur Facebook!</a></li>			
-					</ul>
-				</div>
-			</div>
-      </div>
-    </div>
-      </footer>
 </body>
 </html>
--- interface.php
+++ interface.php
@@ -7,6 +7,10 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="cssGen/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="cssGen/gen.css">
+    <link rel="stylesheet" type="text/css" href="cssGen/compagnons.css">
+    <link rel="stylesheet" type="text/css" href="cssGen/montures.css">
+    <link rel="stylesheet" type="text/css" href="cssGen/carte.css">
+    <link rel="stylesheet" type="text/css" href="cssGen/caracteristiques.css">
 </head>
 
 <body>
@@ -20,23 +24,24 @@
     <div class="row">
         <div class="col-md-12">
             <div class="col-md-2 menu btn-group-vertical">
-                    <button type="button" class="btn btn-default" id="carte">Carte</button>
-                    <button type="button" class="btn btn-default" id="planque">Planque</button>
-                    <button type="button" class="btn btn-default" id="caracteristiques">Caractéristiques</button>
-                    <button type="button" class="btn btn-default" id="compagnon">Compagnons</button>
-                    <button type="button" class="btn btn-default" id="monture">Montures</button>
-                    <button type="button" class="btn btn-default" id="competences">Compétences</button>
-                    <button type="button" class="btn btn-default" id="vendeur">Vendeur</button>
-                    <button type="button" class="btn btn-default" id="equipement">Equipements</button>
-                    <button type="button" class="btn btn-default" id="guilde">Guilde</button>
-                    <button type="button" class="btn btn-default" id="classement">Classement</button>
-                    <button type="button" class="btn btn-default" id="boutique">Boutique</button>
+                    <button type="button" class="btnMenu"  id="carte">Carte</button>
+                    <button type="button" class="btnMenu" id="planque">Planque</button>
+                    <button type="button" class="btnMenu" id="caracteristiques">Caractéristiques</button>
+                    <button type="button" class="btnMenu" id="compagnon">Compagnons</button>
+                    <button type="button" class="btnMenu" id="monture">Montures</button>
+                    <button type="button" class="btnMenu" id="competences">Compétences</button>
+                    <button type="button" class="btnMenu" id="vendeur">Vendeur</button>
+                    <button type="button" class="btnMenu" id="equipement">Equipements</button>
+                    <button type="button" class="btnMenu" id="guilde">Guilde</button>
+                    <button type="button" class="btnMenu" id="classement">Classement</button>
+                    <button type="button" class="btnMenu" id="boutique">Boutique</button>
             </div>
             <?= include_once "includes/default.php" ?>
             <?= include_once "includes/carte.php" ?>
             <?= include_once "includes/compagnons.php" ?>
             <?= include_once "includes/compagnons2.php" ?>
             <?= include_once "includes/montures.php" ?>
+            <?= include_once "includes/caracteristiques.php" ?>
         </div>
     </div>
 
