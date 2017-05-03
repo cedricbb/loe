e9b09ecb6d080b3af39519f4b63ed81604b5e9ce
BruceLyon <marotte.bruce@gmail.com>
2017-05-03 09:54:48 +0200

    monture js del

--- monture.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#monture').click(function () {
-        $('.default').css("display", "none");
-        $('.carte').css("display", "none");
-        $('.monture').css("display", "block");
-        $('.compagnon').css("display", "none");
-        $('.compagnon2').css("display", "none");
-    });
-});
